7a6284bf44aea0a3af95515e7624516be0ef178f
ilhambara <g2a.ilhammubarok@gmail.com>
2021-11-14 20:50:25 +0700

    edit: button imports and restructured dir

---
+++ config/site.js
@@ -0,0 +1,10 @@
+const siteConfig = {
+  socials: {
+    Instagram: "https://www.instagram.com/ofc.xtra/",
+    Facebook: "https://www.facebook.com/groups/1762487333890246",
+    YouTube: "https://www.youtube.com/channel/UCF8XFs88f6xNLbaRz3DjMBw",
+    Steam: "https://steamcommunity.com/groups/xtra10th",
+  },
+};
+
+module.exports = siteConfig;
