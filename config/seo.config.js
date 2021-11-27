cba1ddfa85d59b7e50dcdf718680b55942811864
ilhambara <g2a.ilhammubarok@gmail.com>
2021-11-27 21:25:49 +0700

    update: default seo config

---
+++ config/seo.config.js
@@ -0,0 +1,15 @@
+const SEO = {
+  type: "website",
+  url: "https://test.ofc-xtra.site/",
+  title: "XTRA Official Website",
+  description:
+    "X-TRA Is A Group Of People Graduated From AR-RAHMAT Junior High School. Let's get acquainted and have a good connection with us! Greet us via our social media.",
+  openGraph: {
+    ogImg:
+      "https://raw.githubusercontent.com/official-xtra/dev-test-website/main/public/opengraph.png",
+    width: 1024,
+    height: 576,
+  },
+};
+
+module.exports = SEO;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,46 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "../components/layouts/Layout";
-import theme from "../styles/theme";
+import { DefaultSeo } from "next-seo";
 import "@fontsource/inter";
+import theme from "../styles/theme";
+import Head from "next/head";
+import seo from "../config/seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <DefaultSeo
+        canonical={seo.url}
+        title={seo.title}
+        description={seo.description}
+        openGraph={{
+          type: seo.type,
+          title: seo.title,
+          description: seo.description,
+          site_name: seo.title,
+          images: [
+            {
+              url: seo.openGraph.ogImg,
+              width: seo.openGraph.width,
+              height: seo.openGraph.height,
+              alt: `${seo.title} OG Image`,
+            },
+          ],
+        }}
+      />
+
+      <ChakraProvider resetCSS theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
