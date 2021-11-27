9a0f15eda4397f778e9f534e82c5b52685b7e814
Official XTRA <91373638+ofc-xtra@users.noreply.github.com>
2021-11-27 22:49:39 +0700

    edit: og image problem (5)

--- config/seo.config.js
+++ config/seo.config.js
@@ -3,7 +3,7 @@ const SEO = {
   url: "https://test.ofc-xtra.site",
   title: "XTRA Official Website",
   description:
-    "X-TRA Is A Group Of People Graduated From AR-RAHMAT Junior High School. Let's get acquainted and have a good connection with us! Greet us via our social media.",
+    "X-TRA Is A Group Of People Graduated From AR-RAHMAT Junior High School. Let's have a good connection with us!",
   openGraph: {
     width: 1920,
     height: 1080,
